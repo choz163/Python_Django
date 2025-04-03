c5fe44f600ea6998940c89c4c31fc7e3f1bbb802
Komp <artemshoshura@mail.ru>
2025-04-03 18:49:22 +0400

    реализация функционала шаблонизации и взаимодейсвия с медиа

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from catalog.models import Category, Product
+from django.shortcuts import render, get_object_or_404
 
 def home(request):
-    return render(request, 'home.html')
+    categories = Category.objects.all()  # Получаем все категории
+    products = Product.objects.all()  # Получаем все продукты
+    return render(request, 'home.html', {'category': Category, 'products': products})
 
 def contacts(request):
     return render(request, 'contacts.html')
+
+def category_list(request):
+    category= Category.objects.all()
+    context = {"category": category}
+    return render(request, 'category_list.html', context)
+
+def product_list(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, 'product_list.html', context)
