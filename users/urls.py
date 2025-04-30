8e74ce1117ebff0b8732f672aa66062cd76bd62d
Komp <artemshoshura@mail.ru>
2025-04-30 21:02:05 +0400

    исправление работы кнопок регистрации и входа в шапке меню, а так же кнопки выхода из учетной записи

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import RegisterView, CustomLoginView, CustomLogoutView
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import RegisterView
+
 
 app_name = 'users'
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('login/',  LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='/users/login/'), name='logout'),
+
 ]
