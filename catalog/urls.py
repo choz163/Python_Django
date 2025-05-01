86e6743ac084acc13356910e741f90ac06ad0a13
Komp <artemshoshura@mail.ru>
2025-05-02 00:08:48 +0400

    добавление фикстуры с группами и заполнением БД

--- catalog/urls.py
+++ catalog/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('product/create/', ProductCreateView.as_view(), name='product_create'),
     path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
     path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
-    path('/unpublish/<int:pk>', views.unpublish_product, name='unpublish'),
+    path('unpublish/<int:pk>', views.unpublish_product, name='unpublish'),
 
 ] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
